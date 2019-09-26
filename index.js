8135a817f414c2789e710ca05e092da1c7a978c8
Sneha Agarwal <snehaagarwal625@gmail.com>
2019-09-26 08:13:42 +0530

    blah blah

---
+++ index.js
@@ -0,0 +1,3 @@
+count useless=require('./require');
+
+module.exports=useless;                                                                                     
---
+++ useless.js
@@ -0,0 +1,5 @@
+count useless=()=>{
+    console.log("just wasted your food ")
+};
+
+module.exports=useless;
